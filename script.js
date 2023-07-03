1fa689eb315297f45231e14cf441c316e3b03c04
Nuel Elekwachi <elekwaeke300@gmail.com>
2023-07-03 23:09:41 +0100

    updated styling and functionality

--- script.js
+++ script.js
@@ -10,14 +10,23 @@ const checkoutLast = document.querySelector(".checkout_last");
 const cartValue = document.querySelector(".cartNumber");
 const absent = document.querySelector(".absent");
 const filled = document.querySelector(".checkout_filled");
-const image = document.querySelector(".image_product img");
 const mainFirst = document.querySelector(".main_first");
 const shownModal = document.querySelector(".shown_modal");
 const body = document.querySelector("body");
 const hamburger = document.querySelector(".menu");
 const close = document.querySelector(".close");
 const menu = document.querySelector(".links");
+const pTwo = document.createElement("p");
+const deleteButton = document.querySelector(".delete");
+const slider = document.querySelector(".slider");
+const closeSlider = document.querySelector(".slider-close");
+const prev = document.querySelector(".previous");
+const next = document.querySelector(".next");
+const elements = document.querySelectorAll(".product_one");
+const imageChange = document.querySelectorAll(".image_change");
 let number = parseInt(orderNumber.textContent);
+let currentNumber = 0;
+let currentIndex = 0;
 let cartNumber;
 
 minus.addEventListener("click", () => {
@@ -44,13 +53,12 @@ button.addEventListener("click", () => {
 svg.addEventListener("click", () => {
   if (!cartValue.hasChildNodes()) {
     filled.classList.toggle("none");
-    const p = document.createElement("p");
-    p.setAttribute("class", "cart_empty");
-    p.textContent = "Your cart is empty";
-    checkout.appendChild(p);
-    console.log("works");
+    pTwo.setAttribute("class", "cart_empty");
+    pTwo.textContent = "Your cart is empty";
+    checkout.appendChild(pTwo);
   } else {
     filled.classList.remove("none");
+    pTwo.classList.add("none");
   }
   checkout.classList.toggle("none");
 });
@@ -61,4 +69,45 @@ hamburger.addEventListener("click", function () {
 
 close.addEventListener("click", () => {
   menu.classList.remove("active");
+});
+
+deleteButton.addEventListener("click", () => {
+  filled.classList.add("none");
+  pTwo.classList.remove("none");
+  cartNumber.classList.add("none");
+  orderNumber.textContent = 0;
+});
+
+imageChange.forEach((image) => {
+  image.addEventListener("click", () => {
+    slider.classList.remove("none");
+  });
+});
+
+closeSlider.addEventListener("click", () => {
+  slider.classList.add("none");
+});
+
+prev.addEventListener("click", () => {
+  elements[currentIndex].classList.add("none");
+  currentIndex = (currentIndex - 1 + elements.length) % elements.length;
+  elements[currentIndex].classList.remove("none");
+});
+
+prev.addEventListener("click", () => {
+  imageChange[currentNumber].classList.add("none");
+  currentNumber = (currentNumber - 1 + image.length) % imageChange.length;
+  imageChange[currentNumber].classList.remove("none");
+});
+
+next.addEventListener("click", () => {
+  elements[currentIndex].classList.add("none");
+  currentIndex = (currentIndex + 1) % elements.length;
+  elements[currentIndex].classList.remove("none");
+});
+
+next.addEventListener("click", () => {
+  imageChange[currentNumber].classList.add("none");
+  currentNumber = (currentNumber + 1) % imageChange.length;
+  imageChange[currentNumber].classList.remove("none");
 });
